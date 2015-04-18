8685ca03d619847531c0c312cd64f84cd458121d
ljhjimmy <ljhjimmy87@gmail.com>
2015-04-18 23:53:00 +0800

    作業五 15.10 第二版
    新增了註解

--- U10316047_HW4_11_10/src/testMyStack.java
+++ U10316047_HW4_11_10/src/testMyStack.java
@@ -21,37 +21,45 @@ public class testMyStack{
 		
 		System.out.println("\nReverse order:");
 		
-		
+		//While mystack1 isn't empty, invoke pop method to print the element and remove it
 		while (!mystack1.isEmpty()){
 			System.out.println(mystack1.pop());
 		}
+		
+		input.close();
 	}
 }
 
 class MyStack extends ArrayList<Object>{
-
+	
+	//Return if the arraylist is empty or not
 	public boolean isEmpty() {
 		return super.isEmpty();
 	}
 
+	//Return the size
 	public int getSize() {
 		return super.size();
 	}
 
+	//Retrieve the element at the top of the stack without remove it
 	public Object peek() {
 		return super.get(getSize() - 1);
 	}
 	
+	//Remove the top element from the stack and return it
 	public Object pop() {
 		Object o = super.get(getSize() - 1);
 		super.remove(getSize() - 1);
 		return o;
 	}
 
+	//Add the specified element to the stack
 	public void push(Object o) {
 		super.add(o);
 	}
   
+	//Implemented in ArrayList returns a string representation of all the elements in an array list
 	@Override /** Override the toString in the Object class */
 	public String toString() {
 		return "stack: " + super.toString();
